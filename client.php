cbe671ef8201afd3057904da5e50e117ad62230a
Aleix Alcover <clonic79@gmail.com>
2018-11-13 19:12:35 +0100

    sd

--- client.php
+++ client.php
@@ -2,7 +2,7 @@
 
 header("Content-Type: application/json; charset=UTF-8");
 $conn = new mysqli("192.168.1.44", "aleix", "12345", "nautic");
-$stmt = $conn->prepare("SELECT * FROM client inner join vaixell on client.matricula=vaixell.matricula");
+$stmt = $conn->prepare("SELECT client.dni,client.nom FROM client inner join vaixell on client.matricula=vaixell.matricula");
 $stmt->execute();
 $result = $stmt->get_result();
 $outp = $result->fetch_all(MYSQLI_ASSOC);
