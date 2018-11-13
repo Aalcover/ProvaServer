712c2b4d6521e5ecb8373d14a886efd257fbacd9
Aleix Alcover <clonic79@gmail.com>
2018-11-13 18:38:53 +0100

    ccccc5

--- Vaixell.php
+++ Vaixell.php
@@ -2,10 +2,6 @@
 
 header("Content-Type: application/json; charset=UTF-8");
 $conn = new mysqli("10.100.65.33", "aleix", "12345", "nautic");
-$stmt = $conn->prepare("SELECT * FROM vaixell");
-$stmt->execute();
-$result = $stmt->get_result();
-$outp = $result->fetch_all(MYSQLI_ASSOC);
+$result = $conn->query("SELECT * FROM vaixell");
+$outp = $result->fetch_assoc();
 echo json_encode($outp);
-
-?>
