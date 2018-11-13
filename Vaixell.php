6fc291474945c12c9f6f4def706ddb99ded12c27
Aleix Alcover <clonic79@gmail.com>
2018-11-13 18:49:35 +0100

    czz1

--- Vaixell.php
+++ Vaixell.php
@@ -1,7 +1,7 @@
 <?php
 
 header("Content-Type: application/json; charset=UTF-8");
-$conn = new mysqli("10.100.65.33", "aleix", "12345", "nautic");
+$conn = new mysqli("192.168.1.44", "aleix", "12345", "nautic");
 $result = $conn->query("SELECT * FROM vaixell");
 $outp = $result->fetch_assoc();
 echo json_encode($outp);
--- client.php
+++ client.php
@@ -1,11 +1,9 @@
 <?php
 
 header("Content-Type: application/json; charset=UTF-8");
-$conn = new mysqli("10.100.65.33", "aleix", "12345", "nautic");
+$conn = new mysqli("192.168.1.44", "aleix", "12345", "nautic");
 $stmt = $conn->prepare("SELECT * FROM client inner join vaixell on client.matricula=vaixell.matricula");
 $stmt->execute();
 $result = $stmt->get_result();
 $outp = $result->fetch_all(MYSQLI_ASSOC);
 echo json_encode($outp);
-
-?>
