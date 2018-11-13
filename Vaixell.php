6b21d9df7b92e155c45b64657f1e61ffddbbc744
Aleix Alcover <clonic79@gmail.com>
2018-11-13 10:21:30 +0100

    Comit 3

--- Vaixell.php
+++ Vaixell.php
@@ -1,7 +1,7 @@
 <?php
 
 header("Content-Type: application/json; charset=UTF-8");
-$conn = new mysqli("localhost", "aleix", "12345", "nautic");
+$conn = new mysqli("10.100.65.33", "aleix", "12345", "nautic");
 $stmt = $conn->prepare("SELECT * FROM vaixell");
 $stmt->execute();
 $result = $stmt->get_result();
